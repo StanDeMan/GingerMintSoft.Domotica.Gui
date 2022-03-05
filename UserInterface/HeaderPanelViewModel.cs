c03bd711b675c6ea299535ea6224f8f200de2d67
StanDeMan <StanislausE@gmx.de>
2022-03-05 18:16:53 +0100

    First color for header panel

---
+++ UserInterface/HeaderPanelViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ReactiveUI;
+
+namespace GingerMintSoft.Domotica.Gui.ViewModels
+{
+    public class HeaderPanelViewModel : ReactiveObject
+    {
+        // Reference to IScreen that owns the routable view model.
+        public IScreen HostScreen { get; }
+
+        // Unique identifier for the routable view model.
+        public string UrlPathSegment { get; } = "HeaderPanel";
+
+        public HeaderPanelViewModel(IScreen screen) => HostScreen = screen;
+    }
+}
+
