02c6bcd5a2b6a1ba38c0aa77f4594c8dc8960475
StanDeMan <StanislausE@gmx.de>
2022-03-19 02:52:50 +0100

    Path to bitmap converter

---
+++ Converters/PathToBitmapConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using Avalonia;
+using Avalonia.Data.Converters;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+
+namespace GingerMintSoft.Domotica.Gui.Converters
+{
+    public class PathToBitmapConverter : IValueConverter
+    {
+        public static PathToBitmapConverter Instance = new PathToBitmapConverter();
+
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+
+            if (value is string rawUri && targetType.IsAssignableFrom(typeof(Bitmap)))
+            {
+                // Allow for assembly overrides
+                var uri = rawUri.StartsWith("avares://") 
+                    ? new Uri(rawUri) 
+                    : new Uri($"avares://{Assembly.GetEntryAssembly()?.GetName().Name}{rawUri}");
+
+                return new Bitmap(AvaloniaLocator.Current
+                    .GetService<IAssetLoader>()?
+                    .Open(uri));
+            }
+
+            throw new NotSupportedException();
+        }
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
