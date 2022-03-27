a684114239641d3ef46d6603dc451a204321b6a0
StanDeMan <StanislausE@gmx.de>
2022-03-27 12:22:41 +0200

    Quite a lot of fiddling stuff

--- UserInterface/Controls/ToggelButton.axaml.cs
+++ UserInterface/Controls/ToggelButton.axaml.cs
@@ -1,11 +1,16 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Material.Icons.Avalonia;
 
 namespace GingerMintSoft.Domotica.Gui.UserInterface.Controls
 {
     public partial class ToggelButton : UserControl
     {
+        private MaterialIcon _icon = new(); 
+
         public ToggelButton()
         {
             InitializeComponent();
@@ -14,6 +19,12 @@ namespace GingerMintSoft.Domotica.Gui.UserInterface.Controls
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void ButtonOnClick(object? sender, RoutedEventArgs e)
+        {
+            var btnToggle = sender as ToggleButton;
+            btnToggle.
         }
     }
 }
