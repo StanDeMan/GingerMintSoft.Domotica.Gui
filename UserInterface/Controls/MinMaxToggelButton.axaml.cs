5813f1a9c682bde7843421c704676710e2e0f4e7
StanDeMan <StanislausE@gmx.de>
2022-03-27 14:52:54 +0200

    MinMaxToggleButton extension

---
+++ UserInterface/Controls/MinMaxToggelButton.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace GingerMintSoft.Domotica.Gui.UserInterface.Controls
+{
+    public partial class MinMaxToggelButton : UserControl
+    {
+        public MinMaxToggelButton()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
