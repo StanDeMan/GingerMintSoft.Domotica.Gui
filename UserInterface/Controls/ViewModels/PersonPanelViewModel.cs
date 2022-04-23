2348249f844c3b7c1fb56424ba21bd67adf0ea10
StanDeMan <StanislausE@gmx.de>
2022-04-23 09:11:46 +0200

    MVVM persons binding activated

--- UserInterface/Controls/ViewModels/PersonPanelViewModel.cs
+++ UserInterface/Controls/ViewModels/PersonPanelViewModel.cs
@@ -1,15 +1,25 @@
-﻿using GingerMintSoft.Domotica.Gui.Extensions;
-using System.ComponentModel;
+﻿using System.ComponentModel;
+using System.Collections.ObjectModel;
+using GingerMintSoft.Domotica.Gui.Extensions;
+using GingerMintSoft.Domotica.Gui.UserInterface.Controls.Models;
+using Avalonia.Controls;
 
 namespace GingerMintSoft.Domotica.Gui.UserInterface.Controls.ViewModels
 {
-    public class PersonPanelViewModel : INotifyPropertyChanged
+    public class PersonPanelViewModel
     {
+        public ObservableCollection<Person> Persons => new()
+        {
+            new Person("Cyndi", "/Assets/cyndi-lauper.jpg", Person.EnmResidentialState.In),
+            new Person("George", "/Assets/george-clooney.jpg", Person.EnmResidentialState.In),
+            new Person("Harry", "/Assets/harry.jpg", Person.EnmResidentialState.In),
+            new Person("Hermine", "/Assets/hermine.jpg", Person.EnmResidentialState.Out),
+            new Person("Ron", "/Assets/ron.jpg", Person.EnmResidentialState.Out)
+        };
+
+
         public PersonPanelViewModel()
         {
-
         }
-
-        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
--- UserInterface/Controls/Views/PersonPanel.axaml.cs
+++ UserInterface/Controls/Views/PersonPanel.axaml.cs
@@ -11,26 +11,12 @@ namespace GingerMintSoft.Domotica.Gui.UserInterface.Controls.Views
         public PersonPanel()
         {
             InitializeComponent();
-
-            var listBoxPersons = this.FindControl<ListBox>("ListBoxPersons");
-            listBoxPersons.Items = new Persons();
+            DataContext = new PersonPanelViewModel();
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
-        }
-    }
-
-    public class Persons : ObservableCollection<Person>
-    {
-        public Persons()
-        {
-            Add(new Person("Cyndi", "/Assets/cyndi-lauper.jpg", Person.EnmResidentialState.In));
-            Add(new Person("George", "/Assets/george-clooney.jpg", Person.EnmResidentialState.In));
-            Add(new Person("Harry", "/Assets/harry.jpg", Person.EnmResidentialState.In));
-            Add(new Person("Hermine", "/Assets/hermine.jpg", Person.EnmResidentialState.Out));
-            Add(new Person("Ron", "/Assets/ron.jpg", Person.EnmResidentialState.Out));
         }
     }
 }
