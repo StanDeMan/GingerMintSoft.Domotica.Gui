03927061fef0c81d58808303302670e9bb0d904a
StanDeMan <StanislausE@gmx.de>
2022-04-23 09:55:33 +0200

    Put dummy person creation into ctor

--- UserInterface/Controls/ViewModels/PersonPanelViewModel.cs
+++ UserInterface/Controls/ViewModels/PersonPanelViewModel.cs
@@ -5,17 +5,29 @@ namespace GingerMintSoft.Domotica.Gui.UserInterface.Controls.ViewModels
 {
     public class PersonPanelViewModel
     {
-        public ObservableCollection<Person> Persons => new ()
+        // residential persons
+        private ObservableCollection<Person> _persons = new();
+        public ObservableCollection<Person> Persons
         {
-            new Person("Cyndi", "/Assets/cyndi-lauper.jpg", Person.EnmResidentialState.In),
-            new Person("George", "/Assets/george-clooney.jpg", Person.EnmResidentialState.In),
-            new Person("Harry", "/Assets/harry.jpg", Person.EnmResidentialState.In),
-            new Person("Hermine", "/Assets/hermine.jpg", Person.EnmResidentialState.Out),
-            new Person("Ron", "/Assets/ron.jpg", Person.EnmResidentialState.Out)
-        };
+            get => _persons;
+            set => _persons = value;
+        }
+
+        public PersonPanelViewModel(ObservableCollection<Person> persons)
+        {
+            Persons = persons;
+        }
 
         public PersonPanelViewModel()
         {
+            Persons = new ObservableCollection<Person>
+            {
+                new Person("Cyndi", "/Assets/cyndi-lauper.jpg", Person.EnmResidentialState.In),
+                new Person("George", "/Assets/george-clooney.jpg", Person.EnmResidentialState.In),
+                new Person("Harry", "/Assets/harry.jpg", Person.EnmResidentialState.In),
+                new Person("Hermine", "/Assets/hermine.jpg", Person.EnmResidentialState.Out),
+                new Person("Ron", "/Assets/ron.jpg", Person.EnmResidentialState.Out)
+            };
         }
     }
 }
